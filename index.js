dc426fa4474dad326f257607067ed2be313504a8
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-11-09 19:39:42 -0300

    feat: get users route

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("ToDo APP API");
+});
+
+app.get("/users", (req, res) => {
+  res.send(
+    "Rota ativada com GET e recurso Usuário: valores de usuários devem ser retornados."
+  );
 });
 
 app.listen(port, () => {
