624c1f404396d2d08e9a883e3b9d00e0023cb8e7
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-11-10 18:46:42 -0300

    refact: main file into src directory

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 const express = require("express");
+
+const usersHandler = require("./controllers/UsersControllers/UsersControllers");
+const tasksHandler = require("./controllers/TasksControllers/TaskControllers");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("ToDo APP API");
 });
-
-function usersHandler(req, res) {
-  res.send(
-    "Rota ativada com GET e recurso Usuário: valores de usuários devem ser retornados."
-  );
-}
-
-function tasksHandler(req, res) {
-  res.send(
-    "Rota ativada com GET e recurso Tarefas: valores de tarefas devem ser retornados."
-  );
-}
 
 app.get("/users", usersHandler);
 app.get("/tasks", tasksHandler);
