4f28bea1a87f3a37c28b6ae7a20fef47d8d8e54c
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-11-17 18:30:44 -0300

    feat: user model properties

---
+++ src/models/userModel.js
@@ -0,0 +1,11 @@
+const sha256 = require("js-sha256");
+
+class UserModel {
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = sha256(password);
+  }
+}
+
+module.exports = UserModel;
