72f38c89a57396fc424f93a7843cee080cd75dfa
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-11-19 14:39:27 -0300

    refact: added uuid to models

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,5 +1,8 @@
+const { v4: uuid } = require("uuid");
+
 class Tasks {
   constructor(title, description, status) {
+    this.id = uuid();
     this.title = title;
     this.description = description;
     this.status = status;
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,9 @@
+const { v4: uuid } = require("uuid");
 const sha256 = require("js-sha256");
 
 class UserModel {
   constructor(name, email, password) {
+    this.id = uuid();
     this.name = name;
     this.email = email;
     this.password = sha256(password);
