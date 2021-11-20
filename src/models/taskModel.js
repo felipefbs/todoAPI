cfcd6c6c358cfcaef5289b47bc5e71fc66f17a74
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-11-20 17:32:55 -0300

    refact: added user id

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,11 +1,12 @@
 const { v4: uuid } = require("uuid");
 
 class Tasks {
-  constructor(title, description, status) {
+  constructor(title, description, status, userID) {
     this.id = uuid();
     this.title = title;
     this.description = description;
     this.status = status;
+    this.user_id = userID;
     this.created_at = new Date().toLocaleString();
   }
 }
