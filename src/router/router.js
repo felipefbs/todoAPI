ab8974c593df07a725ad4bb0131f5edb46bd45a4
Felipe Ferreira <felipeffbs3x@gmail.com>
2021-11-29 18:35:33 -0300

    feat: added welcome message

--- src/router/router.js
+++ src/router/router.js
@@ -13,6 +13,10 @@ router.use((req, res, next) => {
 
 router.use(express.json());
 
+router.get("/", (req, res) => {
+  res.send("Welcome to ToDo APP API");
+});
+
 router.use("/users", UserRouter);
 router.use("/tasks", TaskRouter);
 
